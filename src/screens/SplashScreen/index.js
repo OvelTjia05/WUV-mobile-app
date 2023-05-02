8991cc00a3bc414cd3d969a457a354883796df7c
Ovel Tjia <oveltjia05@gmail.com>
2023-05-02 20:14:56 +0800

    add features: - splashscreen

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -1,14 +1,34 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {Logo} from '../../assets/icons';
+import LinearGradient from 'react-native-linear-gradient';
 
 const SplashScreen = ({navigate}) => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#3E1A7E', '#682CA2']}
+        style={styles.backGround}
+        start={{x: 1, y: 0}}
+        // start={{x: 1, y: 0}}
+        // end={{x: 0, y: 1}}
+      >
+        <Text>WUV</Text>
+      </LinearGradient>
+      {/* <Logo /> */}
     </View>
   );
 };
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backGround: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
