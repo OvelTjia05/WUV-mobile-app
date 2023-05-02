92420950541cd0e40edcd5428924ed35e531bec3
Ovel Tjia <oveltjia05@gmail.com>
2023-05-02 14:18:39 +0800

    add file project react native

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View} from 'react-native';
+import SplashScreen from './src/screens/SplashScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
